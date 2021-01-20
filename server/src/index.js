6d9500b7609fa5c3b713998b1a4cec7d98170472
castynet <riungemaina@gmail.com>
2021-01-20 13:25:18 +0300

    added the schema, created server instance & 'npm installed'

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    http://localhost:4000
+  `);
+});
